4c99b04bdcef8efe962332e26e29a676289dd093
zhong <959751561@qq.com>
2019-02-17 23:51:49 +0800

    微信点餐

---
+++ src/main/java/com/example/micro_letter_ordering/MicroLetterOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro_letter_ordering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroLetterOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroLetterOrderingApplication.class, args);
+	}
+
+}
